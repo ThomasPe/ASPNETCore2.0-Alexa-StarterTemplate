a5b86e04f854c8895c2d5b2c4f9dbb09e27ef9e0
Thomas Pentenrieder <pentenrieder@medialesson.de>
2017-09-26 16:45:19 +0200

    initial

---
+++ AlexaCore/Controllers/AlexaController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Alexa.NET.Request;
+using Alexa.NET.Request.Type;
+using Alexa.NET.Response;
+using Alexa.NET;
+
+namespace AlexaCore.Controllers
+{
+    /// <summary>
+    /// This is the main controller which will reveive 
+    /// all requests from the Amazon Alexa service
+    /// and provide the necessary response
+    /// </summary>
+    /// <remarks>
+    /// URI will look like this: https://mysite.azurewebsites.net/api/Alexa
+    /// </remarks>
+    [Produces("application/json")]
+    [Route("api/Alexa")]
+    public class AlexaController : Controller
+    {
+        private IConfiguration _config;
+        private string _appid;
+
+        /// <summary>
+        /// Constructor will get the Configuration injected
+        /// and gets the app id from the appsettings.json
+        /// </summary>
+        /// <param name="config"></param>
+        public AlexaController(IConfiguration config)
+        {
+            _config = config;
+            _appid = _config.GetValue<string>("SkillApplicationId");
+        }
+
+        /// <summary>
+        /// This method will receive the acutal post request from Amazon
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public IActionResult HandleSkillRequest([FromBody]SkillRequest input)
+        {
+            // Security check
+            // Only accept requests with known app id
+            if (input.Session.Application.ApplicationId != _appid)
+            {
+                return BadRequest();
+            }
+
+            var requestType = input.GetRequestType();
+            if (requestType == typeof(IntentRequest))
+            {
+                var response = HandleIntents(input);
+                return Ok(response);
+            }
+            else if (requestType == typeof(LaunchRequest))
+            {
+                var speech = new SsmlOutputSpeech();
+                speech.Ssml = "<speak>Launch repsonse</speak>";
+                var finalResponse = ResponseBuilder.Tell(speech);
+                return Ok(finalResponse);
+            }
+            else if (requestType == typeof(AudioPlayerRequest))
+            {
+                var speech = new SsmlOutputSpeech();
+                speech.Ssml = "<speak>Audio player repsonse</speak>";
+                var finalResponse = ResponseBuilder.Tell(speech);
+                return Ok(finalResponse);
+            }
+            return Ok(ErrorResponse());
+        }
+
+        /// <summary>
+        /// This method will handle the different intents
+        /// </summary>
+        /// <param name="input"></param>
+        /// <returns>Ready-to-send SkillResponse</returns>
+        private SkillResponse HandleIntents(SkillRequest input)
+        {
+            var intentRequest = input.Request as IntentRequest;
+            var speech = new SsmlOutputSpeech();
+
+            // check the name to determine what you should do
+            if (intentRequest.Intent.Name.Equals("MyIntent"))
+            {
+                speech.Ssml = "<speak>MyIntent response";
+                return ResponseBuilder.Tell(speech);
+            }
+            else if (intentRequest.Intent.Name.Equals("MySecondIntent"))
+            {
+                speech.Ssml = "<speak>MySecondIntent response";
+                return ResponseBuilder.Tell(speech);
+            }
+            else
+            {
+                return ErrorResponse();
+            }
+        }
+
+        /// <summary>
+        /// A default error message for all unexpected issues
+        /// </summary>
+        /// <returns>Error Message</returns>
+        private SkillResponse ErrorResponse()
+        {
+            var speech = new SsmlOutputSpeech();
+            speech.Ssml = "<speak>I'm sorry, something went wrong.</speak>";
+            return ResponseBuilder.Tell(speech);
+        }
+    }
+}
---
+++ AlexaCore/Startup.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Alexa.NET.Security.Middleware;
+
+namespace AlexaCore
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // requests on all controllers will be validated, so no non-Amazon-Alexa-requests will get past this
+            app.UseAlexaRequestValidation();
+
+            app.UseMvc();
+        }
+    }
+}
